4960254fd1011899b68106b6552d181e31fa2254
Armando <jaimes087.uz@gmail.com>
2020-06-26 21:45:30 -0500

    Updates

---
+++ precache-manifest.7db4d1e9bbbe1c324b1272466b14ed30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdff2ab680dfe5d3c4335f4662927818",
+    "url": "/react-basic-product-app/index.html"
+  },
+  {
+    "revision": "b6119125a7928bcaee72",
+    "url": "/react-basic-product-app/static/css/main.4ef2dd8e.chunk.css"
+  },
+  {
+    "revision": "450f003e97bfea5275f3",
+    "url": "/react-basic-product-app/static/js/2.fc7d61d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-basic-product-app/static/js/2.fc7d61d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6119125a7928bcaee72",
+    "url": "/react-basic-product-app/static/js/main.ffc4492d.chunk.js"
+  },
+  {
+    "revision": "00c2d032e0a5a62d6570",
+    "url": "/react-basic-product-app/static/js/runtime-main.76030f70.js"
+  }
+]);
